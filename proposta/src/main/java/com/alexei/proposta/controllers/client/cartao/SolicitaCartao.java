c69a3c05d41a3ccf381f1a532f74b100efe15324
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-07-31 18:09:44 -0300

    Jaeger adicionado ao projeto

--- proposta/src/main/java/com/alexei/proposta/controllers/client/cartao/SolicitaCartao.java
+++ proposta/src/main/java/com/alexei/proposta/controllers/client/cartao/SolicitaCartao.java
@@ -3,12 +3,12 @@ package com.alexei.proposta.controllers.client.cartao;
 import com.alexei.proposta.controllers.client.documento.SendProposta;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "api-cartoes", url = "http://localhost:8888/api/cartoes")
 public interface SolicitaCartao {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/")
-    CartaoResposta getCartao(SendProposta proposta);    
+    @PostMapping("/")
+    CartaoResposta getCartao(@RequestBody SendProposta proposta);    
 }
